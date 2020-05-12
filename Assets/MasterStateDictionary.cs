bfd444e30ec290a855d8e7743e6e9766277e60c4
Pansimula <54655619+Pansimula@users.noreply.github.com>
2020-05-12 16:03:20 -0400

    Draw calls and other stuff
    Worked on add Input and add State
    Draw calls
    Adding things actually add them to dict now too
    Gets names and names things properly

--- Assets/MasterStateDictionary.cs
+++ Assets/MasterStateDictionary.cs
@@ -7,13 +7,25 @@ public class MasterStateDictionary
     #region singleton
     private static MasterStateDictionary instance;
     public static MasterStateDictionary Instance { get { return instance ?? (instance = new MasterStateDictionary()); } }
-    private MasterStateDictionary() { masterStateDict = new Dictionary<string, List<MethodPackage>>(); }
+    private MasterStateDictionary() { masterStateDict = new Dictionary<string, Dictionary<int,List<MethodPackage>>>(); }
     #endregion
 
-    public Dictionary<string, List<MethodPackage>> masterStateDict { get; private set; }
+    public Dictionary<string, Dictionary<int,List<MethodPackage>>> masterStateDict { get; private set; }
 
-    public void SaveCurrentState(string stateName, List<MethodPackage> methodPkgs)
+    public void SaveCurrentState(Dictionary<string, Dictionary<int, List<MethodPackage>>> toSave)
     {
-
+        masterStateDict = new Dictionary<string, Dictionary<int, List<MethodPackage>>>();
+        foreach (KeyValuePair<string, Dictionary<int, List<MethodPackage>>> kv in toSave)
+        {
+            masterStateDict.Add(kv.Key, new Dictionary<int, List<MethodPackage>>());
+            foreach (KeyValuePair<int, List<MethodPackage>> subDict in kv.Value)
+            {
+                masterStateDict[kv.Key].Add(subDict.Key, new List<MethodPackage>());
+                foreach (MethodPackage mp in subDict.Value)
+                {
+                    masterStateDict[kv.Key][subDict.Key].Add(mp.CreateClone());
+                }
+            }
+        }
     }
 }
--- Assets/UI/UIControl.cs
+++ Assets/UI/UIControl.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class UIControl : MonoBehaviour
 {
@@ -10,19 +11,60 @@ public class UIControl : MonoBehaviour
     public GameObject MainTuringPanel;
     public GameObject HUD;
     public GameObject StatePanel;
-    public UIProgrammingPanel programmingPanel;
+    public GameObject inputPanel;    
     public Text attrDesc;
+    public InputField nameField;
+    public InputField inputNameField;
+    public Button specialAddInputBtn;
+    public Button specialAddStateBtn;
 
     [Header("Resources")]
     public GameObject StateButton;
     public GameObject GenericTuringArguement;
-    
-    [HideInInspector]
-    public Button selectedState;
+    public GameObject InputBtn;
+    public GameObject TuringResourcePanel;
+    public GameObject TuringSlotPrefab;
+
+    [HideInInspector] public Button selectedStateBtn;
+    [HideInInspector] public Button selectedInputStateBtn;
+    int selectedInputState;
+    string selectedState;
+
+    public Dictionary<string, Dictionary<int, List<MethodPackage>>> localMasterDictCopy { get; private set; } //used in UI, to be saved into MasterStateDict
 
     public void Initialize()
     {
         instance = this;
+        //Need to do a deep clone
+        localMasterDictCopy = new Dictionary<string, Dictionary<int, List<MethodPackage>>>();
+        foreach(KeyValuePair<string, Dictionary<int, List<MethodPackage>>> kv in MasterStateDictionary.Instance.masterStateDict)
+        {
+            localMasterDictCopy.Add(kv.Key, new Dictionary<int, List<MethodPackage>>());
+            foreach(KeyValuePair<int, List<MethodPackage>> subDict in kv.Value)
+            {
+                localMasterDictCopy[kv.Key].Add(subDict.Key, new List<MethodPackage>());
+                foreach(MethodPackage mp in subDict.Value)
+                {
+                    localMasterDictCopy[kv.Key][subDict.Key].Add(mp.CreateClone());
+                }
+            }
+        }
+
+
+        //if its empty, create a default one
+        if(localMasterDictCopy.Keys.Count <= 0)
+        {
+            localMasterDictCopy.Add("Queen", new Dictionary<int, List<MethodPackage>>() { { 0, new List<MethodPackage>() } });
+            localMasterDictCopy.Add("Worker", new Dictionary<int, List<MethodPackage>>() { { 0, new List<MethodPackage>() } });
+            localMasterDictCopy.Add("Nurse", new Dictionary<int, List<MethodPackage>>() { { 0, new List<MethodPackage>() } });
+        }
+
+
+        selectedState = localMasterDictCopy.Keys.ToArray()[0];
+        selectedInputState = localMasterDictCopy[selectedState].Keys.ToArray()[0];
+        DrawMainPanel();
+        DrawSidePanel();
+        DrawStateSelectionPanel();
     }
     
     public void FlowUpdate(float dt)
@@ -36,23 +78,171 @@ public class UIControl : MonoBehaviour
         HUD.SetActive(!open);
     }
 
+    public void AddInputBtnPressed()
+    {
+        GameObject newState = GameObject.Instantiate(InputBtn);
+        newState.transform.SetParent(inputPanel.transform);
+        Button b = newState.GetComponent<Button>();
+        newState.name = GetNextAvailableInputNumber().ToString();
+        b.GetComponentInChildren<Text>().text = newState.name;
+        b.onClick.AddListener(() => InputPressed(newState.name, b)); //Want name dynamic, so make function that passes name
+        localMasterDictCopy[selectedState].Add(int.Parse(newState.name), new List<MethodPackage>());
+    }
+
+    public int GetNextAvailableInputNumber()
+    {
+        for(int i = 0; i < 99999; i++)
+        {
+            if (!localMasterDictCopy[selectedState].ContainsKey(i))
+                return i;
+        }
+        Debug.LogError("How was this possibly full? returning -1");
+        return -1;
+    }
+
+    public string GetNextAvailableState()
+    {
+        string[] randomizedArrayOfStateNames = new string[] { "Queen", "Worker", "Nurse", "Road", "Feeder", "Alice", "Bob", "Charlie", "Doggo", "Edward", "Forenzo", "Gilbert", "Homer", "Igor", "J", "k", "l", "M", "N", "O", "P", "Q", "R" };
+        for (int i = 0; i < randomizedArrayOfStateNames.Length; i++)
+        {
+            if (!localMasterDictCopy.ContainsKey(randomizedArrayOfStateNames[i]))
+                return randomizedArrayOfStateNames[i];
+        }
+        Debug.LogError("Took too many default names, chose your own names!");
+        return "Error";
+    }
+
     public void AddStatePressed()
     {
         GameObject newState = GameObject.Instantiate(StateButton);
         newState.transform.SetParent(StatePanel.transform);
+        string newName = GetNextAvailableState();
+        newState.name = newName;
         Button b = newState.GetComponent<Button>();
+        b.GetComponentInChildren<Text>().text = newState.name;
         b.onClick.AddListener(() => StatePressed(newState.name,b)); //Want name dynamic, so make function that passes name
+        localMasterDictCopy.Add(newState.name, new Dictionary<int, List<MethodPackage>>());
+    }
+
+    public void AddSlotPressed()
+    {
+        GameObject go = GameObject.Instantiate(TuringSlotPrefab);
+        go.transform.SetParent(MainTuringPanel.transform);
+        Debug.Log("Forgot to add to dict");
+    }
+
+    public void InputPressed(string inputText, Button b)
+    {
+        selectedInputState = int.Parse(inputText);
+        DrawMainPanel();
     }
 
     public void StatePressed(string stateName, Button b)
     {
-        programmingPanel.LoadState(stateName);
-        selectedState = b;
+        selectedState = stateName;
+        var v = localMasterDictCopy[selectedState].Keys.ToArray();
+        if (v.Length > 0)
+            selectedInputState = v[0];
+        else
+            selectedInputState = -1;
+
+        DrawMainPanel();
+        DrawSidePanel();
     }
 
     public void SavePressed()
     {
-        programmingPanel.SavePressed();
+        MasterStateDictionary.Instance.SaveCurrentState(localMasterDictCopy);
+    }
+
+    public void SelectedInputNameChanged(string newName)
+    {
+        int oldName = selectedInputState;
+        selectedInputState = int.Parse(newName);
+
+        localMasterDictCopy[selectedState].Add(selectedInputState, localMasterDictCopy[selectedState][oldName]);
+        localMasterDictCopy[selectedState].Remove(oldName);
+    }
+
+    public void SelectedStateNameChanged(string newName)
+    {
+        string oldName = selectedState;
+        selectedState = newName;
+        localMasterDictCopy.Add(newName, localMasterDictCopy[oldName]);
+        localMasterDictCopy.Remove(oldName);
+    }
+
+    public void DrawSidePanel()
+    {
+        Debug.Log("Draw inputState sides: " + selectedInputState);
+        //Using selected state and input
+        foreach (Transform t in inputPanel.transform)
+        {
+            if(t != specialAddInputBtn.transform)
+                GameObject.Destroy(t.gameObject);
+        }
+
+
+        foreach(var kv in localMasterDictCopy[selectedState])
+        {
+            GameObject newState = GameObject.Instantiate(InputBtn);
+            newState.transform.SetParent(inputPanel.transform);
+            newState.name = kv.Key.ToString();
+            Button b = newState.GetComponent<Button>();
+            b.GetComponentInChildren<Text>().text = newState.name;
+            b.onClick.AddListener(() => InputPressed(newState.name, b)); //Want name dynamic, so make function that passes name
+        }        
+    }
+
+    public void DrawMainPanel()
+    {
+        Debug.Log("Draw for State: " + selectedState + " and inputState: " + selectedInputState);
+        //Using selected state and input
+        foreach(Transform t in MainTuringPanel.transform)
+        {
+            GameObject.Destroy(t.gameObject);
+        }
+
+        var v = localMasterDictCopy[selectedState];
+        var vv = v.ContainsKey(selectedInputState);
+
+        if (localMasterDictCopy[selectedState].ContainsKey(selectedInputState))
+            foreach (MethodPackage mp in localMasterDictCopy[selectedState][selectedInputState])
+            {
+                GameObject turingSlot = GameObject.Instantiate(TuringSlotPrefab);
+                turingSlot.GetComponent<UITuringSlot>().Initialize(mp.methodName, () => { SlotDeleted(turingSlot); });
+            }
+        
+    }
+
+    public void DrawStateSelectionPanel()
+    {
+        Debug.Log("Draw inputState sides: " + selectedInputState);
+        //Using selected state and input
+        foreach (Transform t in StatePanel.transform)
+        {
+            if (t != specialAddStateBtn.transform)
+                GameObject.Destroy(t.gameObject);
+        }
+
+
+        foreach (var kv in localMasterDictCopy)
+        {
+
+            GameObject newState = GameObject.Instantiate(StateButton);
+            newState.transform.SetParent(StatePanel.transform);
+            newState.name = kv.Key;
+            Button b = newState.GetComponent<Button>();
+            b.GetComponentInChildren<Text>().text = newState.name;
+            b.onClick.AddListener(() => StatePressed(newState.name, b)); //Want name dynamic, so make function that passes name
+        }
+    }
+
+    public void SlotDeleted(GameObject toDelete)
+    {
+
+        GameObject.Destroy(toDelete);
     }
 
 }
+
